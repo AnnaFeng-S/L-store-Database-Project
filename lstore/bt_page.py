15dd822efcdaf4a44c16c13ec27b5994f198d458
Sonic-zzt <59483967+Sonic-zzt@users.noreply.github.com>
2022-02-18 00:37:39 -0800

    Add files via upload

---
+++ lstore/bt_page.py
@@ -0,0 +1,42 @@
+from lstore.page import Page
+
+class Base_Page:
+    def __init__(self, num_columns):
+        self.base_page = []
+        for i in range (0, num_columns):
+            self.base_page.append(Page())
+    
+    def has_capacity(self):
+        return self.base_page[0].has_capacity()
+
+    def write(self, values):
+        for i in range (0, len(self.base_page)):
+            self.base_page[i].write(values[i])
+    
+    def write_col(self, col, value):
+        self.base_page[col].write(value)
+
+    def read(self, index, column):
+        return self.base_page[column].read(index)
+
+    def update(self, index, column, value):
+        self.base_page[column].update(index, value)
+
+class Tail_Page:
+    def __init__(self, num_columns):
+        self.tail_page = []
+        for i in range (0, num_columns):
+            self.tail_page.append(Page())
+    
+    def has_capacity(self):
+        return self.tail_page[0].has_capacity()
+
+    def write(self, values):
+        for i in range (0, len(self.tail_page)):
+            self.tail_page[i].write(values[i])
+
+    def read(self, index, column):
+        return self.tail_page[column].read(index)
+
+    def update(self, index, column, value):
+        self.tail_page[column].update(index, value)
