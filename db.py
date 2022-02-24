7faac2832f1de2b620964bd2ac56c73ef0932a5f
Sonic-zzt <59483967+Sonic-zzt@users.noreply.github.com>
2022-02-24 14:36:16 -0800

    Add files via upload

---
+++ db.py
@@ -0,0 +1,205 @@
+from lstore.table import Table
+from lstore.page_range import Page_Range
+from lstore.bt_page import Base_Page, Tail_Page
+import pickle
+import os
+from lstore.merge_thread import ThreadPool
+import threading
+        
+class BufferPool:
+    
+    def __init__(self):
+        self.bufferpool = []
+        self.bufferpool_list = []
+        self.path = ''
+        
+    def min_used_time(self):
+        Min = float("inf")
+        for page in self.bufferpool:
+            if page.used_time < Min:
+                Min = page.used_time
+                return_value = self.bufferpool.index(page)
+        return return_value
+    
+    def has_capacity(self):
+        if len(self.bufferpool)>=3:
+            return False
+        else:
+            return True
+        
+    def set_path(self, path):
+        self.path = path
+        os.chdir(self.path)
+
+    def memory_to_disk(self, index):
+        Last_saved_base = self.bufferpool[index].meta.Last_saved_base
+        Last_saved_tail = self.bufferpool[index].meta.Last_saved_tail
+        if self.bufferpool[index].dirty == 1:
+            for b_index in range(len(self.bufferpool[index].base_page)):
+                f = open(self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_base_"+ str(b_index) + "_meta.pickle", "wb")
+                pickle.dump(self.bufferpool[index].base_page[b_index].meta_data, f)
+                os.path.join(self.path,self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_base_"+ str(b_index) + "_meta.pickle")
+                f.close()
+                for col_index in range(len(self.bufferpool[index].base_page[b_index].physical_page)):
+                    with open(self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_basepage_"+str(b_index)+"_col_"+str(col_index)+".txt", "wb") as binary_file:
+                        binary_file.write(self.bufferpool[index].base_page[b_index].physical_page[col_index].data)
+                        os.path.join(self.path,
+                                     self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_basepage_"+str(b_index)+"_col_"+str(col_index)+".txt")
+            for t_index in range(Last_saved_tail, len(self.bufferpool[index].tail_page)):
+                f = open(self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_tail_"+ str(t_index) + "_meta.pickle", "wb")
+                pickle.dump(self.bufferpool[index].tail_page[t_index].meta_data, f)
+                os.path.join(self.path,
+                                self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_tail_"+ str(t_index) + "_meta.pickle")
+                f.close()
+                for col_index in range(len(self.bufferpool[index].tail_page[t_index].physical_page)):
+                    with open(self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_tailpage_"+str(t_index)+"_col_"+str(col_index)+".txt", "wb") as binary_file:
+                        binary_file.write(self.bufferpool[index].tail_page[t_index].physical_page[col_index].data)
+                        os.path.join(self.path,
+                                         self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_tailpage_"+str(t_index)+"_col_"+str(col_index)+".txt")
+                self.bufferpool[index].tail_page[b_index].dirty == 0
+                self.bufferpool[index].meta.Last_saved_tail = t_index
+        else:
+            for b_index in range(Last_saved_base, len(self.bufferpool[index].base_page)):
+                if self.bufferpool[index].base_page[b_index].dirty == 1:
+                    f = open(self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_base_"+ str(b_index) + "_meta.pickle", "wb")
+                    pickle.dump(self.bufferpool[index].base_page[b_index].meta_data, f)
+                    os.path.join(self.path,self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_base_"+ str(b_index) + "_meta.pickle")
+                    f.close()
+                    for col_index in range(len(self.bufferpool[index].base_page[b_index].physical_page)):
+                        with open(self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_basepage_"+str(b_index)+"_col_"+str(col_index)+".txt", "wb") as binary_file:
+                            binary_file.write(self.bufferpool[index].base_page[b_index].physical_page[col_index].data)
+                            os.path.join(self.path,
+                                         self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_basepage_"+str(b_index)+"_col_"+str(col_index)+".txt")
+                    self.bufferpool[index].base_page[b_index].dirty == 0
+                    self.bufferpool[index].meta.Last_saved_base = b_index
+            for t_index in range(Last_saved_tail, len(self.bufferpool[index].tail_page)):
+                f = open(self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_tail_"+ str(t_index) + "_meta.pickle", "wb")
+                pickle.dump(self.bufferpool[index].tail_page[t_index].meta_data, f)
+                os.path.join(self.path,
+                                self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_tail_"+ str(t_index) + "_meta.pickle")
+                f.close()
+                for col_index in range(len(self.bufferpool[index].tail_page[t_index].physical_page)):
+                    with open(self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_tailpage_"+str(t_index)+"_col_"+str(col_index)+".txt", "wb") as binary_file:
+                        binary_file.write(self.bufferpool[index].tail_page[t_index].physical_page[col_index].data)
+                        os.path.join(self.path,
+                                        self.bufferpool_list[index][0]+"_"+str(self.bufferpool_list[index][1])+"_tailpage_"+str(t_index)+"_col_"+str(col_index)+".txt")
+                self.bufferpool[index].tail_page[b_index].dirty == 0
+                self.bufferpool[index].meta.Last_saved_tail = t_index   
+        f = open(self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_page_range_meta.pickle",
+                 "wb")
+        pickle.dump(self.bufferpool[index].meta, f)
+        os.path.join(self.path,self.bufferpool_list[index][0] + "_" + str(self.bufferpool_list[index][1]) + "_page_range_meta.pickle")
+        f.close()
+                    
+    def disk_to_memory(self, table_name, page_range):
+        #os.chdir(self.path)
+        #print("Disk to memory: ", page_range)
+        f = open(table_name + "_" + str(page_range) + "_page_range_meta.pickle", "rb")
+        page_range_metadata = pickle.load(f)
+        f.close()
+        return_page_range = Page_Range(page_range_metadata.n_columns,page_range_metadata.next_brid,page_range_metadata.next_trid,page_range_metadata.tail_block_size)
+        return_page_range.meta.next_bpage = page_range_metadata.next_bpage
+        return_page_range.meta.next_tpage = page_range_metadata.next_tpage
+        return_page_range.meta.trid_list = page_range_metadata.trid_list
+        return_page_range.meta.Last_saved_base = page_range_metadata.Last_saved_base
+        return_page_range.meta.Last_saved_tail = page_range_metadata.Last_saved_tail
+        #print("Disk to memory: ", return_page_range.meta.merge_time)
+        for b_index in range(page_range_metadata.next_bpage):
+            return_page_range.base_page.append(Base_Page(return_page_range.meta.n_columns))
+            f = open(table_name + "_" + str(page_range)  + "_base_"+ str(b_index) + "_meta.pickle", "rb")
+            b_metadata = pickle.load(f)
+            return_page_range.base_page[b_index].meta_data = b_metadata
+            f.close()
+            for col_index in range(page_range_metadata.n_columns):
+                #return_page_range.base_page[b_index].physical_page.append(Base_Page.Physical_Page())
+                f = open(table_name + "_" + str(page_range) + "_basepage_" + str(b_index) + "_col_" + str(col_index) + ".txt", "rb")
+                return_page_range.base_page[b_index].physical_page[col_index].data = bytearray(f.read())
+                return_page_range.base_page[b_index].physical_page[col_index].num_records = return_page_range.base_page[b_index].meta_data.num_records
+                f.close()
+        for t_index in range(page_range_metadata.next_tpage):
+            return_page_range.tail_page.append(Tail_Page(return_page_range.meta.n_columns))
+            f = open(table_name + "_" + str(page_range)  + "_tail_"+ str(t_index) + "_meta.pickle", "rb")
+            t_metadata = pickle.load(f)
+            return_page_range.tail_page[t_index].meta_data = t_metadata
+            f.close()
+            for col_index in range(page_range_metadata.n_columns):
+                f = open(table_name + "_" + str(page_range) + "_tailpage_" + str(t_index) + "_col_" + str(col_index) + ".txt", "rb")
+                return_page_range.tail_page[t_index].physical_page[col_index].data = bytearray(f.read())
+                return_page_range.tail_page[t_index].physical_page[col_index].num_records = return_page_range.tail_page[t_index].meta_data.num_records
+                f.close()
+        return return_page_range
+    
+
+class Database():
+
+    def __init__(self):
+        self.tables = []
+        self.table_directory = {}
+        self.bufferpool = BufferPool()
+        self.path = ''
+        self.pool = ThreadPool()
+        self.lock = threading.Lock()
+        print("Database created")
+        pass
+
+    # Not required for milestone1
+    def open(self, path):
+        self.path = path
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.bufferpool.set_path(path)
+        
+
+    def close(self):
+        self.pool.wail_complete()
+        for table in self.tables:
+            f = open(table.name+'_directory','wb')
+            pickle.dump(table.directory,f)
+            os.path.join(table.name+'_directory')
+            f.close()
+            f = open(table.name+'_index','wb')
+            pickle.dump(table.index,f)
+            os.path.join(table.name+'_index')
+            f.close()
+        for i in range(len(self.bufferpool.bufferpool)):
+            if self.bufferpool.bufferpool[i].dirty == 1:
+                self.bufferpool.memory_to_disk(i)
+
+
+    """
+    # Creates a new table
+    :param name: string         #Table name
+    :param num_columns: int     #Number of Columns: all columns are integer
+    :param key: int             #Index of table key in columns
+    """
+    def create_table(self, name, num_columns, key_index):
+        print("Table " + name + " created")
+        table = Table(name, num_columns, key_index, self.pool, self.lock, self.bufferpool)
+        self.tables.append(table)
+        self.table_directory[name] = len(self.tables) - 1
+        return table
+
+    """
+    # Deletes the specified table
+    """
+    def drop_table(self, name):
+        print("Table " + name + " dropped")
+        self.tables.pop(self.table_directory[name])
+
+    """
+    # Returns table with the passed name
+    """
+    def get_table(self, name):
+        f = open(name+'_index','rb')
+        index = pickle.load(f)
+        f.close()
+        table = Table(name, index.table_num_columns, index.table_key, self.pool, self.lock, self.bufferpool)
+        table.index = index
+        f = open(name+'_directory','rb')
+        directory = pickle.load(f)
+        f.close()
+        table.directory = directory
+        self.tables.append(table)
+        self.table_directory[name] = len(self.tables) - 1
+        return table
+        
---
+++ query.py
@@ -0,0 +1,235 @@
+from lstore.table import Table, Record
+from lstore.index import Index
+from lstore.page import Page
+from lstore.db import Database
+import pickle
+
+
+class Query:
+    """
+    # Creates a Query object that can perform different queries on the specified table 
+    Queries that fail must return False
+    Queries that succeed should return the result or True
+    Any query that crashes (due to exceptions) should return False
+    """
+
+    def __init__(self, table):
+        self.table = table
+        pass
+
+    """
+    # internal Method
+    # Read a record with specified RID
+    # Returns True upon succesful deletion
+    # Return False if record doesn't exist or is locked due to 2PL
+    """
+
+    def delete(self, primary_key):
+        rids = self.table.index.locate(0, primary_key)
+        if rids is None:
+            return False
+        rid = rids[0]
+        [Page_Range, Page, Row] = self.table.directory[rid]
+        if [self.table.name, Page_Range] in self.table.bufferpool.bufferpool_list:
+            temp_page_range = self.table.bufferpool.bufferpool[
+                self.table.bufferpool.bufferpool_list.index([self.table.name, Page_Range])]
+            temp_page_range.pin += 1
+        elif self.table.bufferpool.has_capacity() == True:
+            self.table.bufferpool.bufferpool_list.append([self.table.name, Page_Range])
+            temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+            self.table.bufferpool.bufferpool.append(temp_page_range)
+            temp_page_range.pin += 1
+        else:
+            temp_index = self.table.bufferpool.min_used_time()
+            #if self.table.bufferpool.bufferpool[temp_index].dirty == 1:
+            self.table.bufferpool.memory_to_disk(temp_index)
+            temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+            self.table.bufferpool.bufferpool_list[temp_index] = [self.table.name, Page_Range]
+            self.table.bufferpool.bufferpool[temp_index] = temp_page_range
+            temp_page_range.pin += 1
+        temp_page_range.b_delete(Page, Row)
+        temp_page_range.used_time += 1
+        temp_page_range.base_page[Page].dirty = 1
+        self.table.index.delete(self.table.key, primary_key, rid)
+        self.table.directory.pop(rid)
+        temp_page_range.pin -= 1
+        return True
+
+    """
+    # Insert a record with specified columns
+    # Return True upon succesful insertion
+    # Returns False if insert fails for whatever reason
+    """
+
+    def insert(self, *columns):
+        check = 0
+        if self.table.rid == 0:
+            self.table.new_page_range()
+            check = 1
+        elif self.table.page_range_list[-1].has_capacity() == False:
+            self.table.new_page_range()
+            check = 1
+        if check == 1:
+            if self.table.bufferpool.has_capacity() == True:
+                self.table.bufferpool.bufferpool.append(self.table.page_range_list[-1])
+                self.table.bufferpool.bufferpool_list.append([self.table.name, len(self.table.page_range_list) - 1])
+                temp_page_range = self.table.bufferpool.bufferpool[-1]
+            else:
+                temp_index = self.table.bufferpool.min_used_time()
+                #if self.table.bufferpool.bufferpool[temp_index].dirty == 1:
+                self.table.bufferpool.memory_to_disk(temp_index)
+                self.table.bufferpool.bufferpool[temp_index] = self.table.page_range_list[-1]
+                self.table.bufferpool.bufferpool_list[temp_index] = [self.table.name, len(self.table.page_range_list) - 1]
+                temp_page_range = self.table.bufferpool.bufferpool[temp_index]
+        self.table.page_range_list[-1].pin += 1
+        [rid, page_index, index] = self.table.page_range_list[-1].b_write(columns)
+        self.table.page_range_list[-1].base_page[page_index].dirty = 1
+        self.table.index.insert(self.table.key, columns[0], rid)
+        self.table.directory[rid] = [len(self.table.page_range_list) - 1, page_index, index]
+        self.table.page_range_list[-1].used_time += 1
+        self.table.page_range_list[-1].pin -= 1
+        return True
+
+    """
+    # Read a record with specified key
+    # :param key: the key value to select records based on
+    # :param query_columns: what columns to return. array of 1 or 0 values.
+    # Returns a list of Record objects upon success
+    # Returns False if record locked by TPL
+    # Assume that select will never be called on a key that doesn't exist
+    """
+
+    def select(self, index_key, column, query_columns):
+        rids = self.table.index.locate(column, index_key)
+        return_list = []
+        for rid in rids:
+            [Page_Range, Page, Row] = self.table.directory[rid]
+            if [self.table.name, Page_Range] in self.table.bufferpool.bufferpool_list:
+                temp_page_range = self.table.bufferpool.bufferpool[
+                    self.table.bufferpool.bufferpool_list.index([self.table.name, Page_Range])]
+                temp_page_range.pin += 1
+            elif self.table.bufferpool.has_capacity() == True:
+                self.table.bufferpool.bufferpool_list.append([self.table.name, Page_Range])
+                temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+                self.table.bufferpool.bufferpool.append(temp_page_range)
+                temp_page_range.pin += 1
+            else:
+                temp_index = self.table.bufferpool.min_used_time()
+                #if self.table.bufferpool.bufferpool[temp_index].dirty == 1:
+                self.table.bufferpool.memory_to_disk(temp_index)
+                temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+                self.table.bufferpool.bufferpool_list[temp_index] = [self.table.name, Page_Range]
+                self.table.bufferpool.bufferpool[temp_index] = temp_page_range
+                temp_page_range.pin += 1
+            temp_page_range.used_time += 1
+            record = temp_page_range.b_read(Page, Row)
+            columns = []
+            for i in range(self.table.num_columns):
+                if query_columns[i] == 1:
+                    columns.append(record[i])
+                else:
+                    columns.append(None)
+            return_list.append(Record(rid, 0, columns))
+            temp_page_range.pin -= 1
+        return return_list
+
+    """
+    # Update a record with specified key and columns
+    # Returns True if update is succesful
+    # Returns False if no records exist with given key or if the target record cannot be accessed due to 2PL locking
+    """
+
+    def update(self, primary_key, *columns):
+        rids = self.table.index.locate(0, primary_key)
+        if rids is None:
+            return False
+        rid = rids[0]
+        [Page_Range, Page, Row] = self.table.directory[rid]
+        if [self.table.name, Page_Range] in self.table.bufferpool.bufferpool_list:
+            temp_page_range = self.table.bufferpool.bufferpool[
+                self.table.bufferpool.bufferpool_list.index([self.table.name, Page_Range])]
+            temp_page_range.pin += 1
+        elif self.table.bufferpool.has_capacity() == True:
+            self.table.bufferpool.bufferpool_list.append([self.table.name, Page_Range])
+            temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+            self.table.bufferpool.bufferpool.append(temp_page_range)
+            temp_page_range.pin += 1
+        else:
+            temp_index = self.table.bufferpool.min_used_time()
+            #if self.table.bufferpool.bufferpool[temp_index].dirty == 1:
+            self.table.bufferpool.memory_to_disk(temp_index)
+            temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+            self.table.bufferpool.bufferpool_list[temp_index] = [self.table.name, Page_Range]
+            self.table.bufferpool.bufferpool[temp_index] = temp_page_range
+            temp_page_range.pin += 1
+        temp_page_range.used_time += 1
+        if (temp_page_range.tail_has_capacity() == False):
+            self.table.new_tail_page(Page_Range)
+        temp_page_range.base_page[Page].dirty = 1
+        #test_data = temp_page_range.b_read(Page, Row)
+        #print("Data before Update: ", test_data)
+        #print("Indirection before Update: ", temp_page_range.base_page[0].meta_data.read_INDIRECTION(0))
+        temp_page_range.t_update(Page, Row, columns)
+        temp_page_range.tail_page[-1].dirty = 1
+        #test_data = temp_page_range.b_read(Page, Row)
+        #print("Data after Update: ", test_data)
+        #print("Indirection after Update: ", temp_page_range.base_page[0].meta_data.read_INDIRECTION(0))
+        temp_page_range.pin -= 1
+        return True
+
+    """
+    :param start_range: int         # Start of the key range to aggregate 
+    :param end_range: int           # End of the key range to aggregate 
+    :param aggregate_columns: int  # Index of desired column to aggregate
+    # this function is only called on the primary key.
+    # Returns the summation of the given range upon success
+    # Returns False if no record exists in the given range
+    """
+
+    def sum(self, start_range, end_range, aggregate_column_index):
+        rids = self.table.index.locate_range(start_range, end_range, self.table.key)
+        if rids is None:
+            return False
+        return_sum = 0
+        for rid in rids:
+            [Page_Range, Page, Row] = self.table.directory[rid]
+            if [self.table.name, Page_Range] in self.table.bufferpool.bufferpool_list:
+                temp_page_range = self.table.bufferpool.bufferpool[
+                    self.table.bufferpool.bufferpool_list.index([self.table.name, Page_Range])]
+                temp_page_range.pin += 1
+            elif self.table.bufferpool.has_capacity() == True:
+                self.table.bufferpool.bufferpool_list.append([self.table.name, Page_Range])
+                temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+                self.table.bufferpool.bufferpool.append(temp_page_range)
+                temp_page_range.pin += 1
+            else:
+                temp_index = self.table.bufferpool.min_used_time()
+                #if self.table.bufferpool.bufferpool[temp_index].dirty == 1:
+                self.table.bufferpool.memory_to_disk(temp_index)
+                temp_page_range = self.table.bufferpool.disk_to_memory(self.table.name, Page_Range)
+                self.table.bufferpool.bufferpool_list[temp_index] = [self.table.name, Page_Range]
+                self.table.bufferpool.bufferpool[temp_index] = temp_page_range
+                temp_page_range.pin += 1
+            temp_page_range.used_time += 1
+            record = temp_page_range.b_read(Page, Row)
+            return_sum += record[aggregate_column_index]
+            temp_page_range.pin -= 1
+        return return_sum
+
+    """
+    incremenets one column of the record
+    this implementation should work if your select and update queries already work
+    :param key: the primary of key of the record to increment
+    :param column: the column to increment
+    # Returns True is increment is successful
+    # Returns False if no record matches key or if target record is locked by 2PL.
+    """
+
+    def increment(self, key, column):
+        r = self.select(key, self.table.key, [1] * self.table.num_columns)[0]
+        if r is not False:
+            updated_columns = [None] * self.table.num_columns
+            updated_columns[column] = r[column] + 1
+            u = self.update(key, *updated_columns)
+            return u
+        return False
